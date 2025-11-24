c5a09fc4a50cf34419eaffc15a8a753c3b3a59a4
Jhoncy6 <jcmdom1231231@gmail.com>
2025-11-24 17:40:31 -0300

    Finalização do projeto

--- src/main/java/model/Servico.java
+++ src/main/java/model/Servico.java
@@ -1,23 +1,12 @@
 package model;
 
 public class Servico {
-    private int id;
     private String nomeServico;
     private double preco;
 
     public Servico(String nomeServico, double preco) {
         this.nomeServico = nomeServico;
         this.preco = preco;
-    }
-
-    public Servico(int id, String nomeServico, double preco) {
-        this.id = id;
-        this.nomeServico = nomeServico;
-        this.preco = preco;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getNomeServico() {
